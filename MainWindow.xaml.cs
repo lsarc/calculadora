1a5f2edca454ed7d1b90ec2bb82225c1e8192824
Lucas S <lucas.saraceni.c@gmail.com>
2022-11-16 21:39:34 -0300

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using calculadora.model;
+using System.Collections;
+using System.Diagnostics;
+
+namespace calculadora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var pilha = new StackModel();
+
+            pilha.Push(20);
+            pilha.Push(10);
+            pilha.Push(50);
+            pilha.Push(90);
+            Trace.WriteLine(pilha.Pop());
+            pilha.Switch(0, 2);
+            Trace.WriteLine(pilha.Pop());
+            Trace.WriteLine(pilha.Pop());
+            Trace.WriteLine(pilha.PrintStack());
+
+
+        }
+    }
+}
---
+++ model/StackModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.ComponentModel;
+using System.Windows;
+
+namespace calculadora.model
+{
+    public class StackModel
+    {
+        private int start;
+        private int size;
+        private Complex[] stack;
+
+        public StackModel()
+        {
+            start = 0;
+            size = 0;
+            stack = new Complex[100];
+        }
+
+        public void Push(Complex num)
+        {
+            size++;
+            stack[start++] = num;
+        }
+
+        public Complex Pop()
+        {
+            size--;
+            return stack[--start];  
+        }
+
+        public void Switch(int index1, int index2)
+        {
+            var temp = stack[index1];
+            stack[index1] = stack[index2];
+            stack[index2] = temp;
+        }
+
+        public string PrintStack()
+        {
+            string str = string.Empty;
+            for (int i = 0; i < size; i++)
+            {
+                 str += stack[i].ToString() + "\n";
+               
+            }
+            return str;
+        }
+    }
+
+    
+}
